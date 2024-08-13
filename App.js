a49eb2cd4adebf8f484b3c52d4352f585fba47c7
S-Devisri01 <157806034+S-Devisri01@users.noreply.github.com>
2024-08-13 20:20:02 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+# React.JS-Day1
+
+// App.js
+import React from 'react';
+import ChakraComponent from './ChakraComponent';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <ChakraComponent />
+    </div>
+  );
+}
+
+export default App;
