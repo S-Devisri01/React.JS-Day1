9c5925eea18b5d8293f00cecb4309ad8fff202de
S-Devisri01 <157806034+S-Devisri01@users.noreply.github.com>
2024-08-13 20:30:27 +0530

    ChakraComponent.js

---
+++ ChakraComponent.js
@@ -0,0 +1,28 @@
+   React Day_1
+
+// ChakraComponent.js
+import React from 'react';
+import { Box, Button, Text } from '@chakra-ui/react';
+
+function ChakraComponent() {
+  return (
+    <Box
+      maxW="md"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={4}
+      boxShadow="lg"
+      bg="white"
+    >
+      <Text fontSize="xl" mb={4}>Welcome to Chakra UI</Text>
+      <Text mb={4}>
+        Chakra UI is a simple, modular and accessible component library that provides
+        all the building blocks you need to build your React applications.
+      </Text>
+      <Button colorScheme="teal">Get Started</Button>
+    </Box>
+  );
+}
+
+export default ChakraComponent;
