b3229b73672e1a118273a557633bf11a3f76b297
S-Devisri01 <157806034+S-Devisri01@users.noreply.github.com>
2024-08-13 20:33:35 +0530

    theme.js

---
+++ theme.js
@@ -0,0 +1,16 @@
+# React Day_1
+
+// theme.js
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      900: "#1a202c",
+      800: "#2d3748",
+      700: "#4a5568",
+    },
+  },
+});
+
+export default theme;
